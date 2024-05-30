b176d73f4837e7195901c37b31d61886cd079b4e
Arthri <41360489+a@users.noreply.github.com>
2024-05-30 03:42:25 +0000

    Initial files

---
+++ Z.cs
@@ -0,0 +1,12 @@
+﻿extern alias DI;
+global using System.IO.Compression;
+using System.ComponentModel;
+
+internal static class Z
+{
+    internal static void X()
+    {
+        Console.WriteLine(default(ZipArchive));
+        Console.WriteLine(default(LocalizableAttribute));
+    }
+}
